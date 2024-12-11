9e0cae32b597dba9e655b78579423089df4f3a02
Mario St-Gelais <mario.stg@videotron.ca>
2024-12-10 22:02:26 -0500

    add more fields to plant profile form

--- project/forms.py
+++ project/forms.py
@@ -24,45 +24,45 @@ class PlantProfileForm(forms.ModelForm):
             "stratification_duration",  #
             "sowing_depth",  #
             "sowing_period",
-            "sharing_priority",
-            "harvesting_start",
-            "harvesting_end",
-            "harvesting_indicator",
-            "harvesting_mean",
-            "seed_head",
-            "remove_non_seed_material",
-            "viability_test",
-            "seed_storage",
-            "one_cultivar",
-            "packaging_measure",
-            "dormancy",
-            "seed_preparation",
-            "hyperlink",
-            "envelope_label_link",
-            "harvesting_video_link",
-            "seed_picture_link",
-            "pods_seed_head_picture_link",
-            "seed_storage_label_info",
-            "notes",
-            "germinate_easy",
-            "rock_garden",
-            "rain_garden",
-            "pond_edge",
-            "shoreline_rehab",
-            "container_suitable",
-            "ground_cover",
-            "garden_edge",
-            "woodland_garden",
-            "wind_break_hedge",
-            "erosion_control",
-            "seed_availability",
-            "keystones_species",
-            "draught_tolerent",
-            "salt_tolerent",
-            "deer_tolerent",
-            "easy_to_contain",
+            "sharing_priority",  #
+            "harvesting_start",  #
+            "harvesting_end",  #
+            "harvesting_indicator",  #
+            "harvesting_mean",  #
+            "seed_head",  #
+            "remove_non_seed_material",  #
+            "viability_test",  #
+            "seed_storage",  #
+            "one_cultivar",  #
+            "packaging_measure",  #
+            "dormancy",  #
+            "seed_preparation",  #
+            "hyperlink",  #
+            "envelope_label_link",  #
+            "harvesting_video_link",  #
+            "seed_picture_link",  #
+            "pods_seed_head_picture_link",  #
+            "seed_storage_label_info",  #
+            "notes",  #
+            "germinate_easy",  #
+            "rock_garden",  #
+            "rain_garden",  #
+            "pond_edge",  #
+            "shoreline_rehab",  #
+            "container_suitable",  #
+            "ground_cover",  #
+            "garden_edge",  #
+            "woodland_garden",  #
+            "wind_break_hedge",  #
+            "erosion_control",  #
+            "seed_availability",  #
+            "keystones_species",  #
+            "draught_tolerent",  #
+            "salt_tolerent",  #
+            "deer_tolerent",  #
+            "easy_to_contain",  #
             "flower_color",
-            "habit",
+            "habit",  #
         ]
 
     def __init__(self, *args, **kwargs):
