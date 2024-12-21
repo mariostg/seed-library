fc8903bb381cbda1481f1acb0289f5dd560d7598
Mario St-Gelais <mario.stg@videotron.ca>
2024-12-21 00:23:20 -0500

    remove console.log statements

--- static/js/availability-toggler.js
+++ static/js/availability-toggler.js
@@ -9,7 +9,6 @@
 
 function set_availability(elem) {
   if (elem) {
-    console.log("elem:", elem);
     fetch("/toggle-availability/" + elem.dataset.pk, {
       method: "GET",
       headers: {
@@ -18,13 +17,10 @@ function set_availability(elem) {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         // elem.children[2].innerHTML = data.availability;
         if (data.availability || data.availability == "True") {
           elem.classList.add("ok");
-          console.log("YES " + data.availability);
         } else {
-          console.log("NO " + data.availability);
           elem.classList.remove("ok");
         }
       });
