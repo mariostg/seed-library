af9cdb75d985ebae37e70c8a4c4b9010b2945acb
Mario St-Gelais <mario.stg@videotron.ca>
2025-06-21 18:50:03 -0400

    Fix: Update image source to use growth_habit instead of habit in plant search results

--- project/filters.py
+++ project/filters.py
@@ -19,6 +19,100 @@ class PlantProfileFilter(django_filters.FilterSet):
         method="filter_accepting_seed",
     )
 
+    bird_friendly = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    butterfly_friendly = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    boulevard_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    cedar_hedge_replacement = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    container_suitable = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    drought_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    germinate_easy = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    ground_cover = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+
+    color_blue = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_green = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_orange = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_pink = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_purple = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_red = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_white = django_filters.CharFilter(
+        method="filter_color",
+    )
+    color_yellow = django_filters.CharFilter(
+        method="filter_color",
+    )
+    growth_habit = django_filters.CharFilter(
+        method="filter_growth_habit",
+    )
+    flowering_plant = django_filters.CharFilter(
+        method="filter_growth_habit",
+    )
+    grass = django_filters.CharFilter(
+        method="filter_growth_habit",
+    )
+    shrub = django_filters.CharFilter(
+        method="filter_growth_habit",
+    )
+    tree = django_filters.CharFilter(
+        method="filter_growth_habit",
+    )
+    hummingbird_friendly = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    juglone_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    keystones_species = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    limestone_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    nitrogen_fixer = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    produces_burs = django_filters.CharFilter(
+        method="filter_excludes",
+    )
+    salt_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    sand_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    shoreline_rehab = django_filters.CharFilter(
+        method="filter_boolean",
+    )
+    transplantation_tolerant = django_filters.CharFilter(
+        method="filter_boolean",
+    )
     latin_name = django_filters.CharFilter(
         method="filter_icontains",
     )
@@ -49,10 +143,7 @@ class PlantProfileFilter(django_filters.FilterSet):
         choices=utils.MONTHS,
         method="filter_gte",
     )
-    germinate_easy = django_filters.BooleanFilter(
-        field_name="germinate_easy",
-        method="filter_bool",
-    )
+
     max_height = django_filters.NumberFilter(
         method="filter_lte",
     )
@@ -69,6 +160,19 @@ class PlantProfileFilter(django_filters.FilterSet):
             | Q(**{"english_name__icontains": value})
             | Q(**{"french_name__icontains": value})
         )
+
+    def filter_growth_habit(self, queryset, name, value):
+        if value:
+            return queryset.filter(growth_habit__growth_habit=value)
+
+        else:
+            return queryset.none()
+
+    def filter_color(self, queryset, name, value):
+        if value:
+            return queryset.filter(flower_color__flower_color=value)
+        else:
+            return queryset.none()
 
     def filter_seed_availability(self, queryset, name, value):
         if value:
@@ -97,8 +201,14 @@ class PlantProfileFilter(django_filters.FilterSet):
     def filter_latin_name(self, queryset, name, value):
         return queryset.filter(**{"latin_name": value})
 
-    def filter_bool(self, queryset, name, value):
-        return queryset.filter(**{name: value})
+    def filter_boolean(self, queryset, name, value):
+        if value:
+            return queryset.filter(**{name: True})
+        else:
+            return queryset.exclude(**{name: True})
+
+    def filter_excludes(self, queryset, name, value):
+        return queryset.filter(**{name: False})
 
     class Meta:
         model = models.PlantProfile
