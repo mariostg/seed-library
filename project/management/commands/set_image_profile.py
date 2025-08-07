5fb5fb75cc09b64679b3fcd4708d612aa531ccc7
Mario St-Gelais <mario.stg@videotron.ca>
2025-08-07 14:42:33 -0400

    feat: enhance image profile processing by copying images and updating plant profiles

--- project/management/commands/set_image_profile.py
+++ project/management/commands/set_image_profile.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 from django.conf import settings
@@ -9,55 +10,73 @@ from project import models
 class Command(BaseCommand):
     help = "Set the image profile for each plant profile based on the latin name"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "directory",
+            type=Path,
+            help="The directory containing plant images to be processed.",
+        )
+
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS("Starting to set image profiles..."))
-        plants = models.PlantProfile.objects.all()
-        if not plants.exists():
-            self.stdout.write(
-                self.style.ERROR("No plant profiles found to update image profile.")
-            )
+        self.unfound_latin_names = []
+        directory = kwargs["directory"]
+        if not directory.is_dir():
+            self.stdout.write(self.style.ERROR("Invalid directory path."))
             exit(1)
-
-        for plant in plants:
-            self.set_image_profile(plant)
-        self.stdout.write(self.style.SUCCESS("Image profiles updated successfully."))
-
-    def set_image_profile(self, plant_profile: models.PlantProfile):
-        plant_latin_name = plant_profile.latin_name
-        if not plant_latin_name:
-            return
-
-        # Define the directory to search in
-        image_dir = Path(settings.BASE_DIR) / Path("static/images/plants/profile")
-
-        # Check if directory exists
-        if not image_dir.exists():
+        self.stdout.write(self.style.SUCCESS(f"Processing images in {directory}..."))
+        image_paths = list(directory.glob("*.jpg"))
+        image_paths += list(directory.glob("*.JPG"))
+        self.set_all_has_image_false()
+        for image_path in image_paths:
+            self.process_image(image_path)
+        if self.unfound_latin_names:
             self.stdout.write(
-                self.style.WARNING(f"Image directory {image_dir} does not exist")
+                self.style.WARNING(
+                    "The following latin names were not found in the database:\n"
+                    + "\n".join(self.unfound_latin_names)
+                )
             )
-            return
 
-        # Check for any files that start with the plant's latin name
-        matching_files = list(image_dir.glob(f"{plant_latin_name}*"))
+    def set_all_has_image_false(self):
+        # Set has_image_profile to False for all plant profiles
+        models.PlantProfile.objects.all().update(has_image_profile=False)
+        self.stdout.write(
+            self.style.SUCCESS(
+                "All plant profiles have been set to not have an image profile."
+            )
+        )
 
-        # Update has_image_profile based on whether matching files were found
-        has_image = plant_profile.has_image_profile
-        plant_profile.has_image_profile = len(matching_files) > 0
-        plant_profile.save()
-        if has_image != plant_profile.has_image_profile:
+    def _split_image_name(self, image_path: Path):
+        # image name is made of the latin name, followed by an underscore and the author name
+        plant_latin_name, author = image_path.stem.split("_")
+        plant_latin_name = plant_latin_name.strip()
+        author = author.strip()
+        return plant_latin_name, author
+
+    def process_image(self, image_path: Path):
+        plant_latin_name, author = self._split_image_name(image_path)
+        try:
+            plant_profile = models.PlantProfile.objects.get(latin_name=plant_latin_name)
+            # if plant_profile exists, set has_image_profile to True and copy the image to the media directory
+            plant_profile.has_image_profile = True
+            plant_profile.save()
+            # rename the image file to match the plant profile's latin name
+            destination_dir = settings.BASE_DIR / "static/images/plants/profile"
+            new_image_path = destination_dir / f"{plant_latin_name}-profile.jpg"
+            # Create destination directory if it doesn't exist
+            destination_dir.mkdir(parents=True, exist_ok=True)
+            # Copy the image file to the destination
+            shutil.copy2(image_path, new_image_path)
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"Updated has_image_profile for {plant_latin_name} to {plant_profile.has_image_profile}"
+                    f"Image {image_path.name} processed and saved as {new_image_path.name}."
                 )
             )
-        else:
+        except models.PlantProfile.DoesNotExist:
+            self.unfound_latin_names.append(plant_latin_name)
+        except Exception as e:
             self.stdout.write(
-                self.style.NOTICE(
-                    f"No change in image profile for {plant_latin_name}, remains {plant_profile.has_image_profile}"
+                self.style.ERROR(
+                    f"An error occurred while processing {image_path.name}: {e}"
                 )
-            )
-
-        if plant_profile.has_image_profile:
-            self.stdout.write(
-                self.style.SUCCESS(f"Found profile image for {plant_latin_name}")
             )
