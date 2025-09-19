f395aa6b13276f897912f74ec7c9615dc60f4f98
Mario St-Gelais <mario.stg@videotron.ca>
2025-09-18 23:00:25 -0400

    fix: Enhance plant label info with light range, moisture range, and unpack sowing notes

--- project/utils.py
+++ project/utils.py
@@ -39,9 +39,50 @@ def single_plant(pk, request: HttpRequest):
     return plant
 
 
-def plant_label_info(pk, request: HttpRequest):
+def plant_light_range(plant: PlantProfile):
+    if plant.full_sun and plant.full_shade:
+        return "Full Sun to Full Shade"
+    elif plant.full_sun and plant.part_shade:
+        return "Full Sun to Part Shade"
+    elif plant.part_shade and plant.full_shade:
+        return "Part Shade to Full Shade"
+    elif plant.full_sun:
+        return "Full Sun"
+    elif plant.part_shade:
+        return "Part Shade"
+    elif plant.full_shade:
+        return "Full Shade"
+    return "Unknown Light Range"
+
+
+def plant_moisture_range(plant: PlantProfile):
+    if plant.moisture_dry and plant.moisture_wet:
+        return "Dry to Wet"
+    elif plant.moisture_dry and plant.moisture_medium:
+        return "Dry to Medium"
+    elif plant.moisture_medium and plant.moisture_wet:
+        return "Medium to Wet"
+    elif plant.moisture_dry:
+        return "Dry"
+    elif plant.moisture_medium:
+        return "Medium"
+    elif plant.moisture_wet:
+        return "Wet"
+    return "Unknown Moisture Range"
+
+
+def plant_sowing_notes(plant: PlantProfile):
+    if plant.sowing_notes:
+
+        note_list = plant.sowing_notes.split(".")
+        note_list = [note.strip() for note in note_list if note.strip()]
+        return note_list
+    return []
+
+
+def plant_label_info(plant: PlantProfile, request: HttpRequest) -> list[str]:
     detail = None
-    plant = single_plant(pk, request)
+
     if not plant.stratification_detail:
         plant.stratification_detail = "No Stratification"
     if plant.double_dormancy:
@@ -50,8 +91,11 @@ def plant_label_info(pk, request: HttpRequest):
         plant.latin_name,
         plant.english_name,
         plant.french_name,
-        str(plant.max_height) + " feet",
-        f"Bloom: {plant.bloom_start} - {plant.bloom_end}",
+        plant_light_range(plant),
+        f"Moisture: {plant_moisture_range(plant)}",
+        f"{str(plant.max_height)}' tall, {str(plant.max_width)}' wide",
+        f"Bloom: {MONTHS.get(plant.bloom_start, '')} - {MONTHS.get(plant.bloom_end, '')}",
+        *plant_sowing_notes(plant),  # Unpack the list items individually
         plant.stratification_detail,
         plant.sowing_depth.sowing_depth,
     ]
