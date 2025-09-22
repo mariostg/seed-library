7a2ac5df0e44925091607f17ba1be5d2b867f0b7
Mario St-Gelais <mario.stg@videotron.ca>
2025-09-22 18:47:14 -0400

    feat: add utility functions for filter toggling in util.js and include script in base.html

---
+++ static/js/util.js
@@ -0,0 +1,18 @@
+function toggleFilter(toggleId) {
+  current_element = event.target;
+  current_element.classList.toggle("expanded");
+  current_element.classList.toggle("collapser");
+  const toggleMe = document.querySelector(toggleId);
+  toggleMe.classList.toggle("collapsed");
+}
+function toggleFilterWrapper() {
+  const filterWrapper = document.getElementById("filter-wrapper");
+  const buttonText = document.querySelector("#aside-toggler button");
+  if (filterWrapper.style.display === "none") {
+    filterWrapper.style.display = "flex";
+    buttonText.innerHTML = "Hide Filters";
+  } else {
+    filterWrapper.style.display = "none";
+    buttonText.innerHTML = "Show Filters";
+  }
+}
