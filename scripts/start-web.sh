01592dcca6ed4bb82f15edf604facf29ec3f3b8c
Mario St-Gelais <mario.stg@videotron.ca>
2026-07-23 21:40:43 -0400

    feat: Update Gunicorn timeout setting in start-web.sh script

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -7,4 +7,4 @@ touch logs/django.log logs/project.log logs/email.log logs/signals.log
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-exec gunicorn main.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers ${WEB_CONCURRENCY:-3} --timeout 120
+exec gunicorn main.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers ${WEB_CONCURRENCY:-3} --timeout ${GUNICORN_TIMEOUT:-300}
